84dc1f21963bde27dbe2f247bc3104374332e3bd
ryosyntaxx <riodwi12174@gmail.com>
2023-11-15 16:25:35 +0700

    optimalization return response

--- src/main/java/com/ryo/elrys/controller/Accounts.java
+++ src/main/java/com/ryo/elrys/controller/Accounts.java
@@ -1,12 +1,15 @@
 package com.ryo.elrys.controller;
 
 import com.ryo.elrys.model.DTO.AccountsDTO;
+import com.ryo.elrys.response.Response;
 import com.ryo.elrys.service.AccountsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
@@ -16,9 +19,16 @@ public class Accounts {
     AccountsService accountsService;
 
     @PostMapping("/register")
-    public ResponseEntity<String> register (AccountsDTO accountsDTO){
-        accountsService.register(accountsDTO);
-        return ResponseEntity.ok("ok");
-    }
+    public ResponseEntity<Object> register (AccountsDTO accountsDTO){
+        try{
+            return ResponseEntity.ok()
+                    .body(accountsService.register(accountsDTO));
+        } catch (IOException e) {
 
+            return ResponseEntity.badRequest()
+                    .body("Bad");
+        }
+
+
+    }
 }
--- src/main/java/com/ryo/elrys/service/AccountsService.java
+++ src/main/java/com/ryo/elrys/service/AccountsService.java
@@ -1,20 +1,16 @@
 package com.ryo.elrys.service;
 
 import com.ryo.elrys.elastic.Api;
-import com.ryo.elrys.elastic.Client;
-import com.ryo.elrys.elastic.Request;
 import com.ryo.elrys.model.DTO.AccountsDTO;
 //import com.ryo.elrys.repository.AccountsRepository;
 import com.ryo.elrys.model.MAP.AccountsMAP;
-import org.apache.http.HttpHost;
-import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.rest.RestStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.Random;
 
@@ -23,7 +19,7 @@ public class AccountsService {
 
     @Autowired
     Api api;
-    public void register(AccountsDTO accountsDTO){
+    public RestStatus register(AccountsDTO accountsDTO) throws IOException {
 
         Random random = new Random();
 
@@ -35,16 +31,11 @@ public class AccountsService {
                 .times(String.valueOf(new Timestamp(System.currentTimeMillis())))
                 .build();
 
-
-        try {
             IndexResponse response =
                     api.client().index(
                     api.request(accountsMAP), RequestOptions.DEFAULT);
 
-        } catch (Exception err){
-            System.out.println(err);
-        }
-
+            return response.status();
 
     }
 }
